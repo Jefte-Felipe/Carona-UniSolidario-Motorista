61b3dd2cd5e3cd4a08659b75558878b7692f8f54
jefte <jefte.turatto@luby.software>
2023-11-21 10:03:31 -0300

    Ajuste de cor.

--- app/src/main/java/com/tcc/unisolidariamotorista/activities/ProfileActivity.kt
+++ app/src/main/java/com/tcc/unisolidariamotorista/activities/ProfileActivity.kt
@@ -122,7 +122,7 @@ class ProfileActivity : AppCompatActivity() {
             Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_LONG).show()
         }
         else {
-            Toast.makeText(this, "Tarefa cancelada", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Atividade cancelada", Toast.LENGTH_LONG).show()
         }
 
     }
